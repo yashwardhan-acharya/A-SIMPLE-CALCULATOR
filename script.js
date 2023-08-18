bf092db991902ea246b3b12566b9bf39a2277655
Yashwardhan Acharya <118665199+yashwardhan-acharya@users.noreply.github.com>
2023-08-18 16:39:07 +0530

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
-// This function clear all the values
-function clearScreen() {
-    document.getElementById("result").value = "";
-   }
-   
-   // This function display values
-   function display(value) {
-    document.getElementById("result").value += value;
-   }
-   // This function evaluates the expression and return result
-   function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-   }
+function f1() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 1;
+}
+function f2() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 2;
+}
+function f3() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 3;
+}
+function f4() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 4;
+}
+function f5() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 5;
+}
+function f6() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 6;
+}
+function f7() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 7;
+}
+function f8() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 8;
+}
+function f9() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 9;
+}
+function f0() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + 0;
+}
+function fdot() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + ".";
+}
 
-
-  
+function fplus() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + "+";
+}
+function fsub() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + "-";
+}
+function fmul() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + "*";
+}
+function fdivide() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = purani + "/";
+}
+function fres() {
+  purani = document.getElementById("ans").value;
+  document.getElementById("ans").value = eval(purani);
+}
+function fclear() {
+  document.getElementById("ans").value = "";
+}
